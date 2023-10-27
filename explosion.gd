7b2be8fa3fe3775badec7fd398462aaff74f9ddc
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-10-28 04:30:33 +0530

    modded scripts for smoother movement

--- explosion.gd
+++ explosion.gd
@@ -1,16 +1,8 @@
 extends Area2D
 
-
-func explosion():
+func _on_merge_explosion():
 	for o in get_overlapping_bodies():
 		if o is RigidBody2D:
 			var force = (o.global_position - global_position).normalized()
-			force *= 8000
+			force *= 500
 			o.apply_central_impulse(force)
-
-
-func _on_fruit_test():
-	print("explosion")
-	explosion()
-
-
